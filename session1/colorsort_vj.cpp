13a682c69758703c7e1fef0e68828fae61b08988
Vinamra Jain <73946693+vjonline@users.noreply.github.com>
2024-08-25 13:54:05 +0530

    Create colorsort_vj.cpp

---
+++ session1/colorsort_vj.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zero=0;
+        int one=0;
+        int two=0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i]==0)
+            {
+                zero++;
+            }
+            if(nums[i]==1)
+            {
+                one++;
+            }
+        }
+        for(int i=0;i<zero;i++)
+        {
+            nums[i]=0;
+        }
+        for(int i=zero;i<zero+one;i++)
+        {
+            nums[i]=1;
+        }
+        for(int i=zero+one;i<nums.size();i++)
+        {
+            nums[i]=2;
+        }
+    }
+};
