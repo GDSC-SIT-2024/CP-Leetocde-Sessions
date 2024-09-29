0f4608140d63dad0974b3adda9df35760d95aa1a
Om Dhamame <92315316+ohmschrodinger@users.noreply.github.com>
2024-09-29 20:02:25 +0530

    Create 048_Rotate_Array.cpp

---
+++ 048_Rotate_Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        
+        //transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        //reverse
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                swap(matrix[i][j], matrix[i][n - 1 - j]);
+            }
+        }
+    }
+};
